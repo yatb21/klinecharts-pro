d3d4bfa403a161c97675e81efc9c4d95f25c37ba
mr-reb00t <andrei@op.studio>
2025-11-26 16:01:57 +0000

    Add getChart() method - version 0.1.4

--- src/ChartProComponent.tsx
+++ src/ChartProComponent.tsx
@@ -113,7 +113,8 @@ const ChartProComponent: Component<ChartProComponentProps> = props => {
     setSymbol,
     getSymbol: () => symbol(),
     setPeriod,
-    getPeriod: () => period()
+    getPeriod: () => period(),
+    getChart: () => widget
   })
 
   const documentResize = () => {
--- src/KLineChartPro.tsx
+++ src/KLineChartPro.tsx
@@ -128,4 +128,9 @@ export default class KLineChartPro implements ChartPro {
   getPeriod (): Period {
     return this._chartApi!.getPeriod()
   }
+
+
+  getChart () {
+    return this._chartApi!.getChart()
+  }
 }
